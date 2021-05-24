576abced780ad68139b5be1afb10eacd5e5ac922
Qw3rtyone <mathew.aswin@ymail.com>
2021-05-24 13:58:14 +0100

    minor docker container name issue. Should be fixed

--- internal/pkg/db/migrations/mysql/000001_create_todolist_table.up.sql
+++ internal/pkg/db/migrations/mysql/000001_create_todolist_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS Users(
     ID INT NOT NULL UNIQUE AUTO_INCREMENT,
     Title VARCHAR (127) NOT NULL,
     Note VARCHAR (127),
-    Completed NUMBER(1),
+    Completed NUMBER (1),
     PRIMARY KEY (ID)
 )
